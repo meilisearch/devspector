6a04e101aafa407ea791ee6dc37e7bdc9d5e8283
Irevoire <tamo@meilisearch.com>
2022-04-30 14:09:28 +0200

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const projects = fs.readdirSync(path.join(__dirname, "../specifications/text"))
+
+module.exports = {
+  title: 'Welcome to the Meilisearch specifications!',
+  theme: 'default-prefers-color-scheme',
+  themeConfig: {
+    prefersTheme: 'dark',
+    nav: [
+      { text: 'Specification', link: 'specifications/text/0000-specification-template.md' },
+    ],
+    sidebar: projects.map(spec => [`specifications/text/${spec}`, spec.slice(5, -3).replace(/-/g, " ").replace(/^./, spec[5].toUpperCase())]),
+  },
+  plugins: [
+    [
+      "meilisearch",
+      {
+        hostUrl: 'http://localhost:7700',
+        apiKey:
+          'masterKey',
+        indexUid: 'specs',
+        debug: false,
+        enableDarkMode: 'auto'
+        // "maxSuggestions": 10,
+        // placeholder: 'Search as you type...'
+      }
+    ]
+  ]
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Meilisearch and vuepress
+
+Use the search bar to test the plugin!
+
+[I'm a link](/)
